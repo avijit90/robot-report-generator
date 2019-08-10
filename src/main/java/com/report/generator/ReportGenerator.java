dd8d45a6b6c4ba908d8f83b30c5a990caf0e263e
avijit90 <sepiaavi@gmail.com>
2019-08-10 23:09:51 +0800

    Unused dependency removed, and xml serialisation to object added.

--- src/main/java/com/report/generator/ReportGenerator.java
+++ src/main/java/com/report/generator/ReportGenerator.java
@@ -2,23 +2,16 @@ package com.report.generator;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.report.generator.model.Product;
+import com.report.generator.model.Robot;
 import com.report.generator.service.ConfigurationService;
 import com.report.generator.service.DynamicBuilder;
-import com.report.generator.service.ProductStaticBuilder;
+import com.report.generator.service.ProductBuilder;
 import com.report.generator.service.ViewBuilder;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
-import freemarker.template.TemplateException;
-import org.apache.commons.io.FileUtils;
-import org.xhtmlrenderer.pdf.ITextRenderer;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
-
-import static com.report.generator.service.ViewBuilder.OUTPUT_DIRECTORY;
 
 public class ReportGenerator {
 
@@ -28,23 +21,22 @@ public class ReportGenerator {
     }
 
     private void run() throws Exception {
+
         final ObjectMapper objectMapper = new ObjectMapper();
         final ViewBuilder viewBuilder = new ViewBuilder();
         final ConfigurationService configurationService = new ConfigurationService();
-        final ProductStaticBuilder productStaticBuilder = new ProductStaticBuilder();
-        DynamicBuilder dynamicBuilder = new DynamicBuilder();
+        final ProductBuilder productBuilder = new DynamicBuilder();
         Map root = new HashMap();
 
-        //dynamicBuilder.loadObjectIntoMemory();
+        Robot robotRoot = ((DynamicBuilder) productBuilder).loadObjectIntoMemory();
 
         Configuration config = configurationService.getConfiguration();
         Template sidebarTemplate = config.getTemplate("sidebar.ftl");
         Template outputTemplate = config.getTemplate("output.ftl");
 
-        Product ftRecord = productStaticBuilder.getFundTransferRecord();
-        //Product sysFeatures = productStaticBuilder.getSysFeaturesRecord();
-        Product termDeposit = productStaticBuilder.getTermDepositRecord();
-        Product overviewRecord = productStaticBuilder.buildOverview(ftRecord, null, termDeposit);
+        Product ftRecord = productBuilder.getFundTransferRecord();
+        Product termDeposit = productBuilder.getTermDepositRecord();
+        Product overviewRecord = productBuilder.buildOverview(ftRecord, null, termDeposit);
 
         viewBuilder.addStaticValuesToRoot(root);
 
@@ -52,16 +44,6 @@ public class ReportGenerator {
         viewBuilder.createOutputFile(objectMapper, outputTemplate, overviewRecord, root);
         viewBuilder.createOutputFile(objectMapper, outputTemplate, ftRecord, root);
         viewBuilder.createOutputFile(objectMapper, outputTemplate, termDeposit, root);
-
-        /*OutputStream outputStream = new FileOutputStream(OUTPUT_DIRECTORY +"message.pdf");
-        ITextRenderer renderer = new ITextRenderer();
-        String overview = "abc";
-        renderer.setDocumentFromString(overview);
-        renderer.layout();
-        renderer.createPDF(outputStream);
-        outputStream.close();*/
-
-        /*createOutputFile(objectMapper, outputTemplate, sysFeatures);*/
 
         System.out.println("--------------------------------");
 
--- src/main/java/com/report/generator/service/DynamicBuilder.java
+++ src/main/java/com/report/generator/service/DynamicBuilder.java
@@ -1,21 +1,16 @@
 package com.report.generator.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.report.generator.model.Product;
-import com.report.generator.model.robot.Robot;
-import org.w3c.dom.Document;
+import com.report.generator.model.Robot;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
 import java.io.File;
-import java.io.IOException;
 
+import static com.report.generator.constants.ApplicationConstants.OUTPUT_XML;
 import static org.apache.commons.io.FileUtils.getFile;
 
 public class DynamicBuilder implements ProductBuilder {
-
-    public static final String OUTPUT_XML = "./src/main/resources/output.xml";
 
     @Override
     public Product buildOverview(Product ftRecord, Product sysFeatures, Product termDeposit) {
@@ -42,21 +37,14 @@ public class DynamicBuilder implements ProductBuilder {
     }
 
     public Robot loadObjectIntoMemory() {
+
         File file = readFileFromPath(OUTPUT_XML);
 
         try {
-//            ObjectMapper xmlMapper = new XmlMapper();
-//            Robot robot = xmlMapper.readValue(file, Robot.class);
 
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder docBuilder = dbFactory.newDocumentBuilder();
-            Document xmlDom = docBuilder.parse(file);
-            String xmlAsString = xmlDom.toString(); // this will not print what you want
-            System.out.println("XML as String using DOM Parser : ");
-            System.out.println(xmlAsString);
-
-            //return robot;
-            return null;
+            JAXBContext jaxbContext = JAXBContext.newInstance(Robot.class);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            return (Robot) unmarshaller.unmarshal(file);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
