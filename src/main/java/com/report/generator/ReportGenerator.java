dd2a38886dd03221e2251be9d54a3c30457525ad
avijit90 <sepiaavi@gmail.com>
2019-08-13 08:31:32 +0800

    Generate report from static input.

--- src/main/java/com/report/generator/ReportGenerator.java
+++ src/main/java/com/report/generator/ReportGenerator.java
@@ -1,12 +1,10 @@
 package com.report.generator;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
 import com.report.generator.model.Product;
 import com.report.generator.model.Robot;
-import com.report.generator.service.ConfigurationService;
-import com.report.generator.service.DynamicBuilder;
-import com.report.generator.service.ProductBuilder;
-import com.report.generator.service.ViewBuilder;
+import com.report.generator.service.*;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 
@@ -25,25 +23,27 @@ public class ReportGenerator {
         final ObjectMapper objectMapper = new ObjectMapper();
         final ViewBuilder viewBuilder = new ViewBuilder();
         final ConfigurationService configurationService = new ConfigurationService();
-        final ProductBuilder productBuilder = new DynamicBuilder();
+        final ProductBuilder productBuilder = new ProductTestLoader();
         Map root = new HashMap();
 
-        Robot robotRoot = ((DynamicBuilder) productBuilder).loadObjectIntoMemory();
+        //Robot robotRoot = ((DynamicBuilder) productBuilder).loadObjectIntoMemory();
 
         Configuration config = configurationService.getConfiguration();
-        Template sidebarTemplate = config.getTemplate("sidebar.ftl");
-        Template outputTemplate = config.getTemplate("output.ftl");
+        //Template sidebarTemplate = config.getTemplate("sidebar.ftl");
+        Template outputTemplate = config.getTemplate("/advanced/index.ftl");
+        //Template outputTemplate = config.getTemplate("/test.ftl");
 
         Product ftRecord = productBuilder.getFundTransferRecord();
         Product termDeposit = productBuilder.getTermDepositRecord();
-        Product overviewRecord = productBuilder.buildOverview(ftRecord, null, termDeposit);
+        Product sysFeatures = productBuilder.getSysFeaturesRecord();
+        Product overviewRecord = productBuilder.buildOverview(ftRecord, sysFeatures, termDeposit);
 
         viewBuilder.addStaticValuesToRoot(root);
 
-        viewBuilder.createOutputFile(objectMapper, sidebarTemplate, overviewRecord, root);
+        //viewBuilder.createOutputFile(objectMapper, sidebarTemplate, overviewRecord, root);
         viewBuilder.createOutputFile(objectMapper, outputTemplate, overviewRecord, root);
-        viewBuilder.createOutputFile(objectMapper, outputTemplate, ftRecord, root);
-        viewBuilder.createOutputFile(objectMapper, outputTemplate, termDeposit, root);
+        //viewBuilder.createOutputFile(objectMapper, outputTemplate, ftRecord, root);
+        //viewBuilder.createOutputFile(objectMapper, outputTemplate, termDeposit, root);
 
         System.out.println("--------------------------------");
 
