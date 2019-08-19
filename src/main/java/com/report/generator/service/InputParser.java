6c434e40eb399d90100bd8dab451d78e3008be92
avijit90 <sepiaavi@gmail.com>
2019-08-19 22:03:04 +0800

    Changed code to accept input and output parameters.

--- src/main/java/com/report/generator/service/InputParser.java
+++ src/main/java/com/report/generator/service/InputParser.java
@@ -5,7 +5,7 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 
 import static java.text.MessageFormat.format;
-import static org.apache.commons.lang3.ArrayUtils.getLength;
+import static java.util.Arrays.stream;
 import static org.apache.commons.lang3.ArrayUtils.isNotEmpty;
 import static org.apache.commons.lang3.StringUtils.isEmpty;
 
@@ -29,23 +29,29 @@ public class InputParser {
     private void processUserArgs() {
 
         if (isNotEmpty(args)) {
-            inputDir = args[0];
 
-            if (getLength(args) > 1) {
-                outputDir = args[1];
-                File output = FileUtils.getFile(outputDir);
-                if (!output.isDirectory() && !output.canWrite()) {
-                    System.out.println(format("Either the output directory is not present or write " +
-                            "permission is not given for the directory={0}", output));
+            stream(args).forEach(arg -> {
+                if (arg.startsWith("input=")) {
+                    String[] inputArray = arg.split("input=");
+                    inputDir = inputArray.length > 1 ? inputArray[1] : null;
+                } else if (arg.startsWith("output=")) {
+                    String[] outputArray = arg.split("output=");
+                    outputDir = outputArray.length > 1 ? outputArray[1] : null;
+                    File output = FileUtils.getFile(outputDir);
+                    if (!output.isDirectory() && !output.canWrite()) {
+                        System.out.println(format("Either the output directory is not present or write " +
+                                "permission is not given for the directory={0}", output));
+                    }
                 }
-            }
+            });
+
         }
 
         if (isEmpty(inputDir))
-            inputDir = fileService.getInputDirectory();
+            inputDir = fileService.getDefaultInputDirectory();
 
         if (isEmpty(outputDir))
-            outputDir = fileService.getOutputDirectory();
+            outputDir = fileService.getDefaultOutputDirectory();
     }
 
     public String getInputDir() {
