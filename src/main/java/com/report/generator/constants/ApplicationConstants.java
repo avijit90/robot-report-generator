ac81eac137233dd58c295df807a597e6823d76a5
avijit90 <sepiaavi@gmail.com>
2019-08-21 10:23:16 +0800

    Removed unused constants.

--- src/main/java/com/report/generator/constants/ApplicationConstants.java
+++ src/main/java/com/report/generator/constants/ApplicationConstants.java
@@ -1,8 +1,6 @@
 package com.report.generator.constants;
 
 public class ApplicationConstants {
-    public static final String HTML_FILE_EXTENSION = ".html";
-    public static final String UNDERSCORE = "_";
     public static final String REPORT_HTML = "report.html";
     public static final String OUTPUT_XML = "output.xml";
     public static final String LOG_HTML = "log.html";
